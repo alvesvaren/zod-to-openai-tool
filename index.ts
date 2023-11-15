418e4fddd06007febc99edd7421f53815be0ea7c
Alve Svarén <alve.svaren@intunio.se>
2023-11-15 23:10:55 +0100

    Fix type issues and tools

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
-import {z} from 'zod';
+import { z } from "zod";
+import { zodToJsonSchema } from "zod-to-json-schema";
+import type { OpenAI } from "openai";
 
-
-interface Steps<T = void, Omitted extends string = ''> {
-  input<S extends z.ZodType<any, any>>(schema: S): Omit<Steps<z.infer<S>, Omitted | 'input'>, 'input' | Omitted>;
-  run(func: (input: T extends void ? never : T) => unknown): Omit<Steps<T, Omitted | 'run'>, 'run' | 'input' | Omitted>;
-  describe(description: string): Omit<Steps<T, Omitted | 'describe'>, Omitted | 'describe'>;
+interface Steps<T = void, Omitted extends string = never> {
+  input<S extends z.ZodType<any, any>>(schema: S): Omit<Steps<z.infer<S>, Omitted | "input">, "input" | Omitted>;
+  run(func: (input: T extends void ? never : T) => unknown): Omit<Steps<T, Omitted | "run">, "run" | "input" | Omitted> & CompletedTool;
+  describe(description: string): Omit<Steps<T, Omitted | "describe">, Omitted | "describe">;
 }
+
 
 interface Data {
   func: (input: any) => unknown;
@@ -13,7 +15,11 @@ interface Data {
   description: string | undefined;
 }
 
-function tool<T = void>(): Steps<T> & {__getData: () => {func: (input: T extends void ? never : T) => unknown, schema: z.ZodType<any, any>, description: string | undefined}} {
+interface CompletedTool {
+  __getData(): Data;
+}
+
+function tool<T = void>(): Steps<T> & CompletedTool {
   let schema: z.ZodType<any, any>;
   let description: string | undefined;
   let func: (input: T extends void ? never : T) => unknown;
@@ -21,7 +27,7 @@ function tool<T = void>(): Steps<T> & {__getData: () => {func: (input: T extends
   return {
     input(s) {
       schema = s;
-      return this as unknown as Steps<z.infer<typeof s>>;
+      return this;
     },
     run(f) {
       func = f;
@@ -31,10 +37,36 @@ function tool<T = void>(): Steps<T> & {__getData: () => {func: (input: T extends
       description = d;
       return this;
     },
+    /** @internal */
     __getData() {
-      return {schema, func, description};
+      return { schema, func, description };
     },
   };
 }
 
-tool().input(z.string()).run((input) => input.toUpperCase()).describe('Converts a string to uppercase');
+export function tools<T>(t: { [K in keyof T]: CompletedTool }) {
+  type Tool = (typeof t)[keyof T];
+  return Object.entries<Tool>(t).map(([name, tool]) => {
+    const jsonSchema = zodToJsonSchema(tool.__getData().schema);
+    return { name, description: tool.__getData().description, parameters: jsonSchema };
+  });
+}
+
+const thing = tool()
+  .input(z.string())
+  .describe("Converts input to uppercase")
+  .run(input => input.toUpperCase());
+
+console.log(
+  tools({
+    getWeather: tool()
+      .input(z.string())
+      .describe("Gets the weather")
+      .run(async city => {
+        return {
+          weather: "sunny",
+          city,
+        };
+      }),
+  })
+);
